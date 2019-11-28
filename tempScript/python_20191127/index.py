a173507b02052b864a6808e604954aac4e791b06
fengdanbailu <oncwnuEXMiOLANGI2r_DOrfD0hEA@git.weixin.qq.com>
2019-11-28 08:43:26 +0800

    update daily

---
+++ tempScript/python_20191127/index.py
@@ -0,0 +1,12 @@
+arraylist=["audi","bmw","subaru","toyota"]
+print(arraylist)
+arraylist.sort(reverse=False)
+print(arraylist)
+arraylist.sort(reverse=True)
+print(arraylist)
+# sorted
+print(len(arraylist))
+for key in arraylist:
+	print(key)
+	
+print(list(range(1,10)))
